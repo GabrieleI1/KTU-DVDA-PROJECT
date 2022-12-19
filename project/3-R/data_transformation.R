c2135cad63052ed7d1b60e1998a2031876b3a611
GabrieleI1 <116371310+GabrieleI1@users.noreply.github.com>
2022-12-19 18:26:19 +0200

    Add files via upload

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,13 @@
+setwd("~/Desktop/1 semestras/Didziuju duomeni rinkybos metodai/Projektas")
+library(tidyverse)
 library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
+data <- read_csv("1-sample_data.csv")
+select(data,y)
+data_additional2 <- read_csv("2-additional_data.csv")
+data_additional3 <- read_csv("3-additional_features.csv")
+joined_data <- union(data, data_additional2)
+data_full <-joined_data %>% 
+  inner_join(data_additional3, by = "id")
+
+write_csv(data_full, "train_data.csv")
 ### save combined file into 1-data directory
--- project/3-R/modelling.R
+++ project/3-R/modelling.R
@@ -1,2 +1,52 @@
 library(h2o)
+library(tidyverse)
 h2o.init()
+
+df <- h2o.importFile("train_data.csv")
+df
+class(df)
+summary(df)
+
+y<-"y"
+x <- setdiff(names(df), c(y, "id"))
+df$y<- as.factor(df$y)
+summary(df)
+
+splits <- h2o.splitFrame(df, c(0.6,0.2), seed=123)
+train <- h2o.assign(splits[[1]], "train")
+valid <- h2o.assign(splits[[2]], "valid")
+test <- h2o.assign(splits[[3]], "test")
+aml<- h2o.automl(x=x,
+                 y=y, 
+                 training_frame = train, 
+                 validation_frame = valid,
+                 max_runtime_secs = 3000)
+aml@leaderboard
+#auc, 
+model <-aml@leader
+
+h2o.auc(perf_train)
+
+test_data <- h2o.importFile("test_data.csv")
+
+h2o.saveModel(model, "KTU-DVDA-PROJECT-main/project/4-model/", filename = "my_model0.83")
+
+#modelis
+model4 <- h2o.loadModel("KTU-DVDA-PROJECT-main/project/4-model/my_model0.83")
+
+#train= 0.85, validation= 0.832, test= 0.832
+perf_train <- h2o.performance(model4, train = TRUE)
+perf_train
+perf_valid <- h2o.performance(model4, valid = TRUE)
+perf_valid
+perf_test <- h2o.performance(model4, newdata = test)
+perf_test
+predictions <- h2o.predict(model4, test_data)
+predictions 
+
+predictions %>%
+  as_tibble() %>%
+  mutate(id = row_number(), y = p0) %>% 
+  select(id, y) %>%
+  write_csv("KTU-DVDA-PROJECT-main/project/5-predictions/predictions_test_KG.csv")
+h2o.shutdown()
